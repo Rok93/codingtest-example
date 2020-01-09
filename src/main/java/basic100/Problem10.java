6346b85fff1d78bdc069235e45792d5be70f5d56
김경록 <goodboy3421@gmail.com>
2020-01-09 17:21:28 +0900

    기초테스트 문제 10번

---
+++ src/main/java/basic100/Problem10.java
@@ -0,0 +1,11 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char input = sc.nextLine().charAt(0);
+        System.out.println(input);
+    }
+}
