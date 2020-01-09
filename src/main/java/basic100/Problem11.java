0cd4f85a5e914f2a346dd45834de804c5bd29dd3
김경록 <goodboy3421@gmail.com>
2020-01-09 17:21:34 +0900

    기초테스트 문제 11번

---
+++ src/main/java/basic100/Problem11.java
@@ -0,0 +1,11 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float input = sc.nextFloat();
+        System.out.printf("%.6f", input);
+    }
+}
