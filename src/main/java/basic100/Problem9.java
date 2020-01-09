b48c463dc1ef018708c9515b733b05750107877c
김경록 <goodboy3421@gmail.com>
2020-01-09 17:21:22 +0900

    기초테스트 문제 9번

---
+++ src/main/java/basic100/Problem9.java
@@ -0,0 +1,11 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(input);
+    }
+}
