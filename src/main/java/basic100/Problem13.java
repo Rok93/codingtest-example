56c1719cd6069992196b3d110d71873b657b437f
김경록 <goodboy3421@gmail.com>
2020-01-09 17:21:48 +0900

    기초테스트 문제 13번

---
+++ src/main/java/basic100/Problem13.java
@@ -0,0 +1,12 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c1 = sc.next().charAt(0);
+        char c2 = sc.next().charAt(0);
+        System.out.println(c2 + " " + c1);
+    }
+}
