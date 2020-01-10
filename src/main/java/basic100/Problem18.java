f48b088a709792190bf8a18884fde3f892e352f8
김경록 <goodboy3421@gmail.com>
2020-01-10 15:24:19 +0900

    기초테스트 문제 18번

---
+++ src/main/java/basic100/Problem18.java
@@ -0,0 +1,12 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        System.out.println(input.replace("-",""));
+    }
+}
