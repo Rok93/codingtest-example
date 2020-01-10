07a02594c6196a8965c4e2fd914ccd52d1457cc4
김경록 <goodboy3421@gmail.com>
2020-01-10 15:28:31 +0900

    기초테스트 문제 20번

---
+++ src/main/java/basic100/Problem20.java
@@ -0,0 +1,21 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char[] charList;
+        if(input.length() > 2001) {
+            charList = new char[2001];
+        } else {
+            charList = new char[input.length()];
+        }
+
+        for (int i = 0; i < charList.length; i++) {
+            charList[i] = input.charAt(i);
+        }
+        System.out.println(charList);
+    }
+}
