9d7fd3a19d38592e7cafafbd8c702a4f1ebfa8c3
김경록 <goodboy3421@gmail.com>
2020-01-15 09:51:59 +0900

    기초테스트 문제 27번

--- src/main/java/basic100/Problem27.java
+++ src/main/java/basic100/Problem27.java
@@ -1,4 +1,11 @@
 package basic100;
 
+import java.util.Scanner;
+
 public class Problem27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double input = sc.nextDouble();
+        System.out.printf("%.11f" ,input);
+    }
 }
