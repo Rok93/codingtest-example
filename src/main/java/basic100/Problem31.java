a2e9ad0d912a0da68d9a87f2f61cb92d371276cd
김경록 <goodboy3421@gmail.com>
2020-01-15 10:12:06 +0900

    기초테스트 문제 31번

--- src/main/java/basic100/Problem31.java
+++ src/main/java/basic100/Problem31.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class Problem31 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-        String result = Integer.toHexString(input);
-        System.out.println(result.toUpperCase());
+        String input = sc.nextLine();
+        char[] inputs = input.toCharArray();
+
+        int length = inputs.length;
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += (int) (Math.pow(8, length - (i + 1)) * Integer.parseInt(String.valueOf(inputs[i])));
+        }
+
+        System.out.println(sum);
     }
 }
