ad05d2852192491c50d7182ea0324054ca3b90de
김경록 <goodboy3421@gmail.com>
2020-01-15 09:58:50 +0900

    기초테스트 문제 30번

---
+++ src/main/java/basic100/Problem30.java
@@ -0,0 +1,12 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        String result = Integer.toHexString(input);
+        System.out.println(result);
+    }
+}
