400ed643700b91b825ef007effcf5b5fa36824b4
김경록 <goodboy3421@gmail.com>
2020-01-15 10:37:26 +0900

    기초테스트 문제 33번

---
+++ src/main/java/basic100/Problem33.java
@@ -0,0 +1,12 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        char c = input.charAt(0);
+        System.out.println((int) c);
+    }
+}
