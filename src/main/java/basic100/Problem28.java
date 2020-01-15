71435b4c87c84374e5523d6157220cdbcacc63da
김경록 <goodboy3421@gmail.com>
2020-01-15 09:54:10 +0900

    기초테스트 문제 28번

---
+++ src/main/java/basic100/Problem28.java
@@ -0,0 +1,11 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long input = sc.nextLong();
+        System.out.println(input);
+    }
+}
