53c7096e09d0cc9c9455aca840cac0faa418ec95
김경록 <goodboy3421@gmail.com>
2020-01-16 11:24:38 +0900

    기초테스트 문제 39번

---
+++ src/main/java/basic100/Problem39.java
@@ -0,0 +1,13 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        int result = (int)(a / b);
+        System.out.println(result);
+    }
+}
