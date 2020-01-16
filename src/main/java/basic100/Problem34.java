8baea4a7146e91c87d21ef17a3d52161642a5809
김경록 <goodboy3421@gmail.com>
2020-01-16 10:27:26 +0900

    기초테스트 문제 34번

---
+++ src/main/java/basic100/Problem34.java
@@ -0,0 +1,12 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem34 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        char result = (char) input;
+        System.out.println(result);
+    }
+}
