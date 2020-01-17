3c71f4be587f3dad4ee383a8418af8935dc45483
김경록 <goodboy3421@gmail.com>
2020-01-17 15:48:34 +0900

    기초테스트 문제 43번

---
+++ src/main/java/basic100/Problem43.java
@@ -0,0 +1,21 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Problem43 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+        String[] list = input.split(" ");
+
+        long a = Long.parseLong(list[0]);
+        long b = Long.parseLong(list[1]);
+        long c = Long.parseLong(list[2]);
+        double average = ((double)(a + b + c) / 3);
+        long sum = a + b + c;
+
+        System.out.println(sum);
+        System.out.printf("%.1f" ,average);
+    }
+}
