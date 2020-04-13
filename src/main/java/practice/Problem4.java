b5b0d83311837c36982bf17e0c74c60ad396ee3b
김경록 <goodboy3421@gmail.com>
2020-04-13 12:15:01 +0900

    프로그래머스 연습문제풀이 4

---
+++ src/main/java/practice/Problem4.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Problem4 {
+
+    public int solution(String s) {
+//        if (s.charAt(0) == '+') {
+//            return Integer.parseInt(s.substring(1));
+//        }
+//
+//        if (s.charAt(0) == '-') {
+//            return -Integer.parseInt(s.substring(1));
+//        }  부호 신경 쓸 필요 없음 ...!
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
